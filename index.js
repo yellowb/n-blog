26be9d6e60a7af5789e60a9ba23da7d42f4e33d8
yellowb <122517167@qq.com>
2017-07-14 10:30:19 +0800

    add express router

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 "use strict"
 
 const express = require('express');
-
 let app = express();
 
-app.get('/', function(req, res) {
-    res.send('hello, express');
-});
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/users');
 
-app.get('/users/:name', function(req, res) {
-    res.send('hello, ' + req.params.name);
-});
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+
+
+//app.get('/:name', function(req, res) {
+//    res.send('hello /, ' + req.params.name);
+//});
+//
+//app.get('/users/:name', function(req, res) {
+//    res.send('hello /users, ' + req.params.name);
+//});
 
 app.listen(8888);
 
