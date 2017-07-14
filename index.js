bb6d8d41fcb3bc47c5cee6a304a2bdf103b9635e
yellowb <122517167@qq.com>
2017-07-14 17:24:08 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 "use strict"
 
+var path = require('path');
 const express = require('express');
 let app = express();
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use('/', indexRouter);
+
+// log middleware
+app.use('/users', function(req, res, next) {
+    console.log(req.url, '  ' , new Date().toString());
+    next();
+});
 app.use('/users', userRouter);
 
-
-//app.get('/:name', function(req, res) {
-//    res.send('hello /, ' + req.params.name);
-//});
-//
-//app.get('/users/:name', function(req, res) {
-//    res.send('hello /users, ' + req.params.name);
-//});
+//错误处理
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(8888);
 
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:name', function(req, res) {
-    res.send('hello, /users ' + req.params.name);
+    res.render('users', {name: req.params.name});
 });
 
 module.exports = router;
