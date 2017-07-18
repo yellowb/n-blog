382d431cf829fea5b71ad75bc3e97502c2fad17f
yellowb <122517167@qq.com>
2017-07-18 15:31:54 +0800

    add logger

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var flash = require('connect-flash');
 var config = require('config-lite')(__dirname);
 var routes = require('./routes');
 var pkg = require('./package');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 
 var app = express();
 
@@ -55,8 +57,39 @@ app.use(function (req, res, next) {
     next();
 });
 
+// 正常请求的日志
+app.use(expressWinston.logger({
+    transports: [
+        new (winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/success.log'
+        })
+    ]
+}));
 // 路由
 routes(app);
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/error.log'
+        })
+    ]
+}));
+
+// error page
+app.use(function (err, req, res, next) {
+    res.render('error', {
+        error: err
+    });
+});
 
 // 监听端口，启动程序
 app.listen(config.port, function () {
